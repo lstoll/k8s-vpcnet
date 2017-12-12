77f0bbb8d8f64d501e6b85747a3a7c716cc7d676
Lincoln Stoll <lincoln.stoll@gmail.com>
2017-12-12 07:34:15 -0600

    Test a little more
    Validate that we can access the full pool of IP addresses

---
+++ cmd/cni-vpcnet/allocator_test.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/lstoll/k8s-vpcnet/pkg/cni/config"
+	"github.com/lstoll/k8s-vpcnet/pkg/cni/diskstore"
+)
+
+func TestAllocator(t *testing.T) {
+	workDir, err := ioutil.TempDir("", "")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(workDir)
+
+	store, err := diskstore.New("test-store", workDir)
+	if err != nil {
+		t.Fatalf("Error opening disk store [%+v]", err)
+	}
+	defer store.Close()
+
+	alloc := &ipAllocator{
+		eniMap: testMap,
+		store:  store,
+		conf:   &config.CNI{},
+	}
+
+	// try allocating and deallocating max addresses
+
+	for i := 0; i < 6; i++ {
+		_, err := alloc.Get(fmt.Sprintf("abc%d", i))
+		if err != nil {
+			t.Fatalf("Error getting address %d [%+v]", i, err)
+		}
+	}
+
+	_, err = alloc.Get("abc7")
+	if err == nil {
+		t.Fatal("Expected error allocating 7th address")
+	}
+
+	for i := 0; i < 6; i++ {
+		_, err := alloc.Release(fmt.Sprintf("abc%d", i))
+		if err != nil {
+			t.Fatalf("Error getting address %d [%+v]", i, err)
+		}
+	}
+}
